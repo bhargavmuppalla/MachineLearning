65c420164848900946d386b42a0818403bc683b8
bhargavmuppalla <39513300+bhargavmuppalla@users.noreply.github.com>
2022-03-14 13:59:24 -0400

    Update ML_least_square_solver_polynomial.py

--- ML_least_square_solver_polynomial.py
+++ ML_least_square_solver_polynomial.py
@@ -8,16 +8,17 @@ y = array([0.8750,0.0753,-0.7305,-0.5486,0.1903,1.0873,0.2081,-0.7886,-0.5698,0.
 fig = plt.figure()
 ax = fig.add_subplot(1,1,1)
 
-#1a answer
+#applying polyfit function for M(hyperparameter denoting number of parameters) values from 2 to 9
 for i in range(2,10):
     pw = polyfit(x,y,i)
     print(i,"=",pw)
 
 
-#1b answer
+# ideal fit is for M=3 for given dataset. f1 is the polynomial equation
 def f1(x):
     return -0.44355089 * x**3 + 3.26780886 * x**2 + -2.79809363 * x + 0.46130839
 
+#calculating squared errors
 for i in range(len(x)):
     se = (y[i]-f1(x[i]))**2
     print("For x=%f"%(x[i]),"squared error = ",se)
@@ -26,6 +27,7 @@ data = []
 for i in range(len(x)):
     data.append(f1(x[i]))
 
+#Visualization of polynomial curve for M=3
 plt.xlabel("X")
 plt.ylabel("Y")
 plt.plot(x,y)
